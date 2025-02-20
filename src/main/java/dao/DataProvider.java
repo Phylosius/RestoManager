30f059937ae63d2df4c75a3e9e41282da199e8aa
Phylosius <hei.phylosius@gmail.com>
2025-02-20 21:48:29 +0300

    refactor(dao): add id parameter to DataProvider.update()

--- src/main/java/dao/DataProvider.java
+++ src/main/java/dao/DataProvider.java
@@ -7,6 +7,6 @@ public interface DataProvider<entityType, idType> {
     List<entityType> getAll(int page, int pageSize);
     entityType getById(idType id);
     void add(entityType entity);
-    void update(entityType entity);
+    void update(idType id, entityType entity);
     void delete(idType id);
 }
