0c35056ea4b8f23cc0cc23516d677624ef3b6c43
Phylosius <hei.phylosius@gmail.com>
2025-02-20 15:12:06 +0300

    feat(db): added test data creation script

---
+++ src/main/java/db/testdata/sql/insert_test_data.sql
@@ -0,0 +1,19 @@
+\c resto_db
+
+-- DISH INSERTION
+INSERT INTO dish(name, unit_price) VALUES
+    ('Hot dog', 15000.0);
+
+-- INGREDIENT INSERTION
+INSERT INTO ingredient(name, modification_date, unit_price, unit) VALUES
+    ('Saucisse', '2025-01-01 00:00', 15000.0, 'G'),
+    ('Huile', '2025-01-01 00:00', 10000.0, 'L'),
+    ('Oeuf', '2025-01-01 00:00', 1000.0, 'U'),
+    ('Pain', '2025-01-01 00:00', 1000.0, 'U');
+
+-- MAKE UP
+INSERT INTO make_up(dish_id, ingredient_id, ingredient_quantity) VALUES
+    ('1', '1', 100.0),
+    ('1', '2', 0.5),
+    ('1', '3', 1.0),
+    ('1', '4', 1.0);
