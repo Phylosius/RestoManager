5fdfe690447e590562bc69c115c7a9996fa7ec68
Phylosius <hei.phylosius@gmail.com>
2025-02-21 04:33:57 +0300

    feat(test): created integration tests for DAOs

---
+++ src/test/java/DishDAOTest.java
@@ -0,0 +1,106 @@
+import dao.DataSource;
+import dao.DishDAO;
+import dao.IngredientDAO;
+import model.Dish;
+import model.Ingredient;
+import model.MakeUp;
+import model.Unit;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class DishDAOTest {
+
+    private DataSource dataSource;
+    private DishDAO subject;
+    private Dish testDish;
+
+    @BeforeAll
+    public void setUp(){
+        Dotenv dotenv = Dotenv.load();
+
+        dataSource = new DataSource(
+                dotenv.get("DB_USERNAME"),
+                dotenv.get("DB_PASSWORD"),
+                dotenv.get("DB_URL"));
+        subject = new DishDAO(dataSource);
+
+        List<Ingredient> testIngredients = IngredientDAO.getAll(dataSource.getConnection(), 1, 3);
+        List<MakeUp> makeUps = List.of(
+                new MakeUp(testIngredients.get(0), 2d),
+                new MakeUp(testIngredients.get(1), 3d),
+                new MakeUp(testIngredients.get(2), 1d));
+
+        testDish = new Dish("0",
+                "Test Dish", 2000.0d, makeUps);
+
+    }
+
+    @Test
+    @Order(1)
+    public void get_dish_paginated_ok() {
+
+        List<Dish> retrievedFirst = subject.getAll(1, 5);
+
+        assertEquals(1, retrievedFirst.size());
+    }
+
+    @Test
+    @Order(2)
+    public void create_dish_ok() {
+
+        subject.add(testDish);
+
+        Dish retrievedDish = subject.getById(testDish.getId());
+        assertEquals(testDish, retrievedDish);
+    }
+
+    @Test
+    @Order(3)
+    public void update_dish_ok() {
+        testDish.setUnitPrice(0.0d);
+
+        subject.update(testDish.getId(), testDish);
+        Dish retrievedDish = subject.getById(testDish.getId());
+
+        assertEquals(testDish.getUnitPrice(), retrievedDish.getUnitPrice());
+    }
+
+    @Test
+    @Order(4)
+    public void get_dish_ok() {
+
+        Dish retrievedDish = subject.getById(testDish.getId());
+
+        assertEquals(testDish, retrievedDish);
+    }
+
+    @Test
+    @Order(5)
+    public void delete_dish_ok() {
+
+        subject.delete(testDish.getId());
+        Dish retrievedDish = subject.getById(testDish.getId());
+
+        assertNull(retrievedDish.getId());
+    }
+
+    @Test
+    @Order(6)
+    public void check_total_recipe_price_ok() {
+
+        Dish retrievedDish = subject.getById("1");
+        Double totalIngredientCost = retrievedDish.getIngredients().stream()
+                .map(m -> m.getIngredient().getUnitPrice() * m.getQuantity())
+                .reduce(0d, Double::sum);
+
+        assertEquals(5500d, totalIngredientCost);
+    }
+}
---
+++ src/test/java/IngredientDAOTest.java
@@ -0,0 +1,89 @@
+import dao.DataSource;
+import dao.IngredientDAO;
+import model.Ingredient;
+import model.Unit;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class IngredientDAOTest {
+
+    private DataSource dataSource;
+    private IngredientDAO subject;
+    private Ingredient testIngredient;
+
+    @BeforeAll
+    public void setUp(){
+        Dotenv dotenv = Dotenv.load();
+
+        dataSource = new DataSource(
+                dotenv.get("DB_USERNAME"),
+                dotenv.get("DB_PASSWORD"),
+                dotenv.get("DB_URL"));
+        subject = new IngredientDAO(dataSource);
+        testIngredient = new Ingredient("0",
+                "Test Ingredient", LocalDate.now(), 2000.0d, Unit.L);
+
+    }
+
+    @Test
+    @Order(1)
+    public void get_ingredient_paginated_ok() {
+
+        List<Ingredient> retrievedFirst = subject.getAll(1, 5);
+        List<Ingredient> retrievedSecond = subject.getAll(2, 5);
+        List<Ingredient> retrievedThird = subject.getAll(3, 5);
+
+        assertEquals(5, retrievedFirst.size());
+        assertEquals(3, retrievedSecond.size());
+        assertEquals(0, retrievedThird.size());
+    }
+
+    @Test
+    @Order(2)
+    public void create_ingredient_ok() {
+
+        subject.add(testIngredient);
+
+        Ingredient retrievedIngredient = subject.getById(testIngredient.getId());
+        assertEquals(testIngredient, retrievedIngredient);
+    }
+
+    @Test
+    @Order(3)
+    public void update_ingredient_ok() {
+        testIngredient.setUnitPrice(0.0d);
+
+        subject.update(testIngredient.getId(), testIngredient);
+        Ingredient retrievedIngredient = subject.getById(testIngredient.getId());
+
+        assertEquals(testIngredient.getUnitPrice(), retrievedIngredient.getUnitPrice());
+    }
+
+    @Test
+    @Order(4)
+    public void get_ingredient_ok() {
+
+        Ingredient retrievedIngredient = subject.getById(testIngredient.getId());
+
+        assertEquals(testIngredient, retrievedIngredient);
+    }
+
+    @Test
+    @Order(5)
+    public void delete_ingredient_ok() {
+
+        subject.delete(testIngredient.getId());
+
+        Ingredient retrievedIngredient = subject.getById(testIngredient.getId());
+
+        assertNull(retrievedIngredient.getId());
+    }
+}
