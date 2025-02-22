b03aaab10511b72616985ac0f4b24e2259237782
Phylosius <hei.phylosius@gmail.com>
2025-02-22 09:04:35 +0300

    refactor(db): changed default value of id into generate_simple_varchar_id(table_name TEXT)

--- src/main/java/db/migration/sql/create_simple_varchar_id_generator.sql
+++ src/main/java/db/migration/sql/create_simple_varchar_id_generator.sql
@@ -1,15 +1,27 @@
 \c resto_db
 
-CREATE SEQUENCE dish_simple_id_sequence START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE ingredient_simple_id_sequence START WITH 1 INCREMENT BY 1;
-
 CREATE OR REPLACE FUNCTION generate_simple_varchar_id(table_name TEXT)
 RETURNS TEXT AS $$
 DECLARE
+    sequence_name TEXT;
     new_id INT;
+    sequence_exists BOOLEAN;
 BEGIN
-    -- Get the next number from the corresponding sequence
-    EXECUTE format('SELECT NEXTVAL(%L || ''_simple_id_sequence'')', table_name) INTO new_id;
+    -- Build the sequence name dynamically
+    sequence_name := table_name || '_simple_id_sequence';
+
+    -- Check if the sequence exists
+    SELECT EXISTS (
+        SELECT 1 FROM pg_class WHERE relname = sequence_name
+    ) INTO sequence_exists;
+
+    -- If the sequence does not exist, create it
+    IF NOT sequence_exists THEN
+        EXECUTE format('CREATE SEQUENCE %I START WITH 1 INCREMENT BY 1', sequence_name);
+    END IF;
+
+    -- Get the next number from the sequence
+    EXECUTE format('SELECT NEXTVAL(%I)', sequence_name) INTO new_id;
 
     -- Return the ID as a string format "n"
     RETURN new_id::TEXT;
--- src/main/java/db/migration/sql/create_table.sql
+++ src/main/java/db/migration/sql/create_table.sql
@@ -1,29 +1,22 @@
-\c resto_db
+-- DB CONNECTION
+\c resto_db;
 
-CREATE OR REPLACE FUNCTION generate_simple_varchar_id(table_name TEXT)
-RETURNS TEXT AS $$
-DECLARE
-    sequence_name TEXT;
-    new_id INT;
-    sequence_exists BOOLEAN;
-BEGIN
-    -- Build the sequence name dynamically
-    sequence_name := table_name || '_simple_id_sequence';
+-- TYPE CREATION
+CREATE TYPE ingredient_unit AS ENUM ('L', 'G', 'U');
 
-    -- Check if the sequence exists
-    SELECT EXISTS (
-        SELECT 1 FROM pg_class WHERE relname = sequence_name
-    ) INTO sequence_exists;
+-- TABLE CREATION
+-- dish
+CREATE TABLE dish(
+    id VARCHAR(100) PRIMARY KEY DEFAULT generate_simple_varchar_id('dish'),
+    name VARCHAR(100),
+    unit_price FLOAT
+);
 
-    -- If the sequence does not exist, create it
-    IF NOT sequence_exists THEN
-        EXECUTE format('CREATE SEQUENCE %I START WITH 1 INCREMENT BY 1', sequence_name);
-    END IF;
-
-    -- Get the next number from the sequence
-    EXECUTE format('SELECT NEXTVAL(%I)', sequence_name) INTO new_id;
-
-    -- Return the ID as a string format "n"
-    RETURN new_id::TEXT;
-END;
-$$ LANGUAGE plpgsql;
+-- ingredient
+CREATE TABLE ingredient(
+    id VARCHAR(100) PRIMARY KEY DEFAULT generate_simple_varchar_id('ingredient'),
+    name VARCHAR(100),
+    modification_date TIMESTAMP,
+    unit_price FLOAT,
+    unit ingredient_unit
+);
