256ca4362502150ba8f35a5fd1131aea1ea853bc
Phylosius <hei.phylosius@gmail.com>
2025-02-28 12:58:22 +0300

    feat(test): created test for Ingredient

---
+++ src/test/java/IngredientTest.java
@@ -0,0 +1,13 @@
+import model.Ingredient;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class IngredientTest {
+    private Ingredient subject;
+
+    @Test
+    void check_available_quantities() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
