aa2874a80e5ded566c526ac39b8d97fab9680474
Phylosius <hei.phylosius@gmail.com>
2025-02-28 19:02:11 +0300

    feat(test.StockMovementDAOTest): added more tests

--- src/test/java/StockMovementDAOTest.java
+++ src/test/java/StockMovementDAOTest.java
@@ -1,17 +1,25 @@
 import dao.DataSource;
+import dao.IngredientDAO;
 import dao.StockMovementDAO;
 import io.github.cdimascio.dotenv.Dotenv;
 import model.*;
 import org.junit.jupiter.api.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class StockMovementDAOTest {
     private StockMovementDAO subject;
     private DataSource dataSource;
+
+    private LocalDateTime now = LocalDateTime.of(2025, 02, 25, 05, 05);
+
+    private Ingredient ingredient1;
+    private Ingredient ingredient2;
 
     @BeforeAll
     void setUp() {
@@ -22,9 +30,18 @@ public class StockMovementDAOTest {
                 dotenv.get("DB_PASSWORD"),
                 dotenv.get("DB_URL"));
         subject = new StockMovementDAO(dataSource);
+
+        // Test data
+        ingredient1 = new Ingredient("-1", "Sel",
+                now,new Price(2.5, now), Unit.G);
+        ingredient2 = new Ingredient("-2", "Riz",
+                now,new Price(3.5, now), Unit.G);
+        IngredientDAO.save(dataSource.getConnection(), ingredient1);
+        IngredientDAO.save(dataSource.getConnection(), ingredient2);
     }
 
     @Test
+    @Order(1)
     void get_filtered_ordered_and_paginated() {
         Criteria quantityCriteria = new Criteria(LogicalOperator.AND, "quantity", CriteriaOperator.GREATER_THAN, 30d);
         Criteria typeCriteria = new Criteria(LogicalOperator.AND, "type", CriteriaOperator.EQUAL, MovementType.IN);
@@ -36,8 +53,54 @@ public class StockMovementDAOTest {
         assertEquals(3, retrieved.size());
     }
 
+    @Test
+    @Order(2)
+    void create_in_movements_ok() {
+        StockMovement selMovement = new StockMovement(ingredient1, MovementType.IN, 10000d, now);
+        StockMovement rizMovement = new StockMovement(ingredient2, MovementType.IN, 20000d, now);
+
+        subject.saveAll(List.of(selMovement, rizMovement));
+        Criteria dateCriteria = new Criteria(LogicalOperator.AND, "date", CriteriaOperator.NEAR, now, 100d);
+        List<StockMovement> retrieved = subject.getAllByCriteria(List.of(dateCriteria), 1, 5);
+
+        assertTrue(retrieved.size() > 0 && retrieved.size() <= 5);
+        assertTrue(retrieved.contains(selMovement));
+        assertTrue(retrieved.contains(rizMovement));
+    }
+
+    @Test
+    @Order(3)
+    void create_out_movements_ok() {
+        LocalDateTime then = LocalDateTime.of(2025, 02, 25, 15, 05);
+        StockMovement selMovement = new StockMovement(ingredient1, MovementType.OUT, 5000d, then);
+        StockMovement rizMovement = new StockMovement(ingredient2, MovementType.OUT, 15000d, then);
+
+        subject.saveAll(List.of(selMovement, rizMovement));
+        Criteria criteria = new Criteria(LogicalOperator.AND, "type", CriteriaOperator.EQUAL, MovementType.OUT);
+        Criteria dateCriteria = new Criteria(LogicalOperator.AND, "date", CriteriaOperator.NEAR, then, 2000d);
+        List<StockMovement> retrieved = subject.getAllByCriteria(List.of(criteria, dateCriteria), 1, 5);
+
+        assertEquals(2, retrieved.size());
+        assertTrue(retrieved.contains(selMovement));
+        assertTrue(retrieved.contains(rizMovement));
+    }
+
+    @Test
+    @Order(4)
+    void check_total_quantity_ok() {
+
+        Double selQuantity = Ingredient.getAvalaibleQuantity("-1", LocalDateTime.now());
+        Double rizQuantity = Ingredient.getAvalaibleQuantity("-2", LocalDateTime.now());
+
+        assertEquals(5000d, selQuantity);
+        assertEquals(5000d, rizQuantity);
+    }
+
     @AfterAll
     public void tearDown(){
+        IngredientDAO.delete(dataSource.getConnection(), ingredient1.getId());
+        IngredientDAO.delete(dataSource.getConnection(), ingredient2.getId());
+
         dataSource.closeConnection();
     }
 }
