8603b77fbd5075f5bcc1032792bf29cd714f4e62
Phylosius <hei.phylosius@gmail.com>
2025-02-28 14:02:09 +0300

    feat(model): created Ingredient::getAvalaibleQuantity

--- src/main/java/model/Ingredient.java
+++ src/main/java/model/Ingredient.java
@@ -1,9 +1,12 @@
 package model;
 
+import dao.IngredientDAO;
 import dao.PriceDAO;
+import dao.StockMovementDAO;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -25,5 +28,22 @@ public class Ingredient {
     public Price getPrice(LocalDateTime localDateTime) {
         PriceDAO priceDAO = new PriceDAO();
         return priceDAO.getNearbyByDateAndIngredientID(localDateTime, id);
+    }
+
+    public Double getAvalaibleQuantity() {
+        return getAvalaibleQuantity(LocalDateTime.now());
+    }
+
+    public Double getAvalaibleQuantity(LocalDateTime date) {
+        return getAvalaibleQuantity(getId(), date);
+    }
+
+    public static Double getAvalaibleQuantity(String ingredientId, LocalDateTime date) {
+        return getStockInfo(ingredientId, date).getQuantity();
+    }
+
+    public static StockInfo getStockInfo(String ingredientId, LocalDateTime date) {
+        StockMovementDAO stockMovementDAO = new StockMovementDAO();
+        return stockMovementDAO.getStockInfo(ingredientId, date);
     }
 }
