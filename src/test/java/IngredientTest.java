cc54109efd7efa27bcaccdf3bf7640589ace1064
Phylosius <hei.phylosius@gmail.com>
2025-02-28 14:26:06 +0300

    refactor: removed non-necessary code

--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -1,22 +1,18 @@
 import model.Ingredient;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class IngredientTest {
-    private Ingredient subject;
-
     @Test
     void check_available_quantities() {
 
-        Double ouefQuantity = subject.getAvalaibleQuantity("3", LocalDateTime.now());
-        Double painQuantity = subject.getAvalaibleQuantity("4", LocalDateTime.now());
-        Double saucisseQuantity = subject.getAvalaibleQuantity("1", LocalDateTime.now());
-        Double huileQuantity = subject.getAvalaibleQuantity("2", LocalDateTime.now());
+        Double ouefQuantity = Ingredient.getAvalaibleQuantity("3", LocalDateTime.now());
+        Double painQuantity = Ingredient.getAvalaibleQuantity("4", LocalDateTime.now());
+        Double saucisseQuantity = Ingredient.getAvalaibleQuantity("1", LocalDateTime.now());
+        Double huileQuantity = Ingredient.getAvalaibleQuantity("2", LocalDateTime.now());
 
         assertEquals(80d, ouefQuantity);
         assertEquals(30d, painQuantity);
