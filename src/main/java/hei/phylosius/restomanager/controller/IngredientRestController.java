4730d526f5b3642fbb410da68068e75ab01df813
Phylosius <hei.phylosius@gmail.com>
2025-04-04 01:56:54 +0300

    chore: added request params ?priceMinFilter and ?priceMaxFilter on GET /ingredients

--- src/main/java/hei/phylosius/restomanager/controller/IngredientRestController.java
+++ src/main/java/hei/phylosius/restomanager/controller/IngredientRestController.java
@@ -3,7 +3,10 @@ package hei.phylosius.restomanager.controller;
 import hei.phylosius.restomanager.dao.IngredientDAO;
 import hei.phylosius.restomanager.dto.IngredientDTO;
 import hei.phylosius.restomanager.mappers.IngredientMapper;
+import hei.phylosius.restomanager.model.Criteria;
+import hei.phylosius.restomanager.model.CriteriaOperator;
 import hei.phylosius.restomanager.model.Ingredient;
+import hei.phylosius.restomanager.model.LogicalOperator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,18 +29,40 @@ public class IngredientRestController {
     }
 
     @GetMapping
-    public ResponseEntity<?> getIngredients(@RequestParam(required = false) Integer page, @RequestParam(required = false) Integer pageSize) {
+    public ResponseEntity<?> getIngredients(@RequestParam(required = false) Integer page,
+                                            @RequestParam(required = false) Integer pageSize,
+                                            @RequestParam(required = false) Double priceMinFilter,
+                                            @RequestParam(required = false) Double priceMaxFilter) {
         List<IngredientDTO> ingredientDTOSs;
         List<Ingredient> ingredients = new ArrayList<>();
+        List<Criteria> criteria = new ArrayList<>();
+
+        if (priceMinFilter != null) {
+            if (priceMinFilter > -1d) {
+                Criteria minPriceCriteria = new Criteria(LogicalOperator.AND, "p.unit_price", CriteriaOperator.GREATER_OR_EQUAL, priceMinFilter);
+                criteria.add(minPriceCriteria);
+            } else {
+                ResponseEntity.status(400).body("?priceMinFilter should be greater or equal to 0");
+            }
+        }
+
+        if (priceMaxFilter != null) {
+            if (priceMaxFilter > -1d) {
+                Criteria minPriceCriteria = new Criteria(LogicalOperator.AND, "p.unit_price", CriteriaOperator.LESS_OR_EQUAL, priceMaxFilter);
+                criteria.add(minPriceCriteria);
+            } else {
+                ResponseEntity.status(400).body("?priceMaxFilter should be greater or equal to 0");
+            }
+        }
 
         if (page != null) {
             if (pageSize != null) {
-                ingredients = ingredientDAO.getAll(page, pageSize);
+                ingredients = ingredientDAO.getAllByCriteria(criteria, page, pageSize);
             } else {
                 ResponseEntity.status(400).body("?page should be given with ?pageSize");
             }
         } else {
-            ingredients = ingredientDAO.getAll();
+            ingredients = ingredientDAO.getAllByCriteria(criteria);
         }
 
         ingredientDTOSs = ingredients.stream().map(ingredientMapper::toDTO).toList();
