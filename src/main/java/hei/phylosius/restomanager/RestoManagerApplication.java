bd30404d19eda17a4ff99ae5b41d047c85fe09ef
Phylosius <hei.phylosius@gmail.com>
2025-04-03 21:21:53 +0300

    chore: initialized spring-boot project

---
+++ src/main/java/hei/phylosius/restomanager/RestoManagerApplication.java
@@ -0,0 +1,13 @@
+package hei.phylosius.restomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestoManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hei/phylosius/restomanager/RestoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.phylosius.restomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
