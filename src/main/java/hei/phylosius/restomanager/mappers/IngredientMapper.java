ae8ea7dc8476617c579557003ad34a219336d38d
Phylosius <hei.phylosius@gmail.com>
2025-04-04 02:05:48 +0300

    chore(mappers): created IngredientMapper::toEntity

--- src/main/java/hei/phylosius/restomanager/mappers/IngredientMapper.java
+++ src/main/java/hei/phylosius/restomanager/mappers/IngredientMapper.java
@@ -1,16 +1,43 @@
 package hei.phylosius.restomanager.mappers;
 
+import hei.phylosius.restomanager.dao.DataSource;
+import hei.phylosius.restomanager.dao.PriceDAO;
 import hei.phylosius.restomanager.dto.IngredientDTO;
 import hei.phylosius.restomanager.model.Ingredient;
+import hei.phylosius.restomanager.model.Unit;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@AllArgsConstructor
 @Component
 public class IngredientMapper {
+
+    private PriceDAO priceDAO;
+
     public IngredientDTO toDTO(Ingredient ingredient) {
         return new IngredientDTO(
                 ingredient.getId(),
                 ingredient.getName(),
                 ingredient.getPrice().getValue(),
                 ingredient.getModificationDate());
+    }
+
+    public Ingredient toEntity(ResultSet resultSet) {
+        try {
+            Ingredient ingredient = new Ingredient();
+
+            ingredient.setId(resultSet.getString("id"));
+            ingredient.setName(resultSet.getString("name"));
+            ingredient.setModificationDate(resultSet.getTimestamp("modification_date").toLocalDateTime());
+            ingredient.setPrice(priceDAO.getLatestByIngredientID(ingredient.getId()));
+            ingredient.setUnit(Unit.valueOf(resultSet.getString("unit")));
+
+            return ingredient;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
