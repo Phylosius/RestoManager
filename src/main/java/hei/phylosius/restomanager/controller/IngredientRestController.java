bd07fdda9e9949825ff17fb4a89e7642b9d781cb
Phylosius <hei.phylosius@gmail.com>
2025-04-04 01:37:40 +0300

    feat: added request params page and pageSize on GET /ingredients

--- src/main/java/hei/phylosius/restomanager/controller/IngredientRestController.java
+++ src/main/java/hei/phylosius/restomanager/controller/IngredientRestController.java
@@ -1,6 +1,7 @@
 package hei.phylosius.restomanager.controller;
 
 import hei.phylosius.restomanager.dao.IngredientDAO;
+import hei.phylosius.restomanager.dto.IngredientDTO;
 import hei.phylosius.restomanager.mappers.IngredientMapper;
 import hei.phylosius.restomanager.model.Ingredient;
 import org.springframework.http.ResponseEntity;
@@ -9,22 +10,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/ingredients")
 public class IngredientRestController {
 
+    private final IngredientMapper ingredientMapper;
     private IngredientDAO ingredientDAO;
 
-    public IngredientRestController(IngredientDAO ingredientDAO) {
+    public IngredientRestController(IngredientDAO ingredientDAO, IngredientMapper ingredientMapper) {
         this.ingredientDAO = ingredientDAO;
+        this.ingredientMapper = ingredientMapper;
     }
 
     @GetMapping
-    public ResponseEntity<?> getIngredients(IngredientMapper mapper, @RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "2") int pageSize) {
-        List<Ingredient> ingredients = ingredientDAO.getAllByCriteria(List.of(), page, pageSize);
+    public ResponseEntity<?> getIngredients(IngredientMapper mapper, @RequestParam Integer page, @RequestParam Integer pageSize) {
+        List<IngredientDTO> ingredientDTOSs;
+        List<Ingredient> ingredients = new ArrayList<>();
 
-        return ResponseEntity.ok(ingredients.stream().map(mapper::toDTO).toList());
+        if (page != null) {
+            if (pageSize != null) {
+                ingredients = ingredientDAO.getAll(page, pageSize);
+            } else {
+                ResponseEntity.status(400).body("?page should be given with ?pageSize");
+            }
+        } else {
+            ingredients = ingredientDAO.getAll();
+        }
+
+        ingredientDTOSs = ingredients.stream().map(ingredientMapper::toDTO).toList();
+
+        return ResponseEntity.ok(ingredientDTOSs);
     }
 }
