1be68213f32d910fcb037821964e7ca65e2ba2af
Phylosius <hei.phylosius@gmail.com>
2025-04-04 00:07:31 +0300

    chore: added .env variables loading on RestoManagerApplication

--- src/main/java/hei/phylosius/restomanager/RestoManagerApplication.java
+++ src/main/java/hei/phylosius/restomanager/RestoManagerApplication.java
@@ -1,5 +1,6 @@
 package hei.phylosius.restomanager;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestoManagerApplication {
 
     public static void main(String[] args) {
+        loadEnvVariables();
+
         SpringApplication.run(RestoManagerApplication.class, args);
+    }
+
+    public static void loadEnvVariables() {
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("datasource.url", dotenv.get("DB_URL"));
+        System.setProperty("datasource.username", dotenv.get("DB_USERNAME"));
+        System.setProperty("datasource.password", dotenv.get("DB_PASSWORD"));
     }
 
 }
