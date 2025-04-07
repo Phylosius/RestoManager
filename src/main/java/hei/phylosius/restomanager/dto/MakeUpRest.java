17884071c73aeda3ff8a98f4eaf240bf462dcf90
Phylosius <hei.phylosius@gmail.com>
2025-04-07 11:27:52 +0300

    refactor: removed id from MakeUpRest

--- src/main/java/hei/phylosius/restomanager/dto/MakeUpRest.java
+++ src/main/java/hei/phylosius/restomanager/dto/MakeUpRest.java
@@ -10,7 +10,6 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MakeUpRest {
-    private String id;
     private IngredientRest ingredient;
     private Double quantity;
 
