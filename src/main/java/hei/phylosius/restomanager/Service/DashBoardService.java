9bcac379f88ab5b903aabb022f295d3b0829c4db
Phylosius <hei.phylosius@gmail.com>
2025-04-11 02:36:16 +0300

    chore(Service): created DashBoardService::getProcessingTime

--- src/main/java/hei/phylosius/restomanager/Service/DashBoardService.java
+++ src/main/java/hei/phylosius/restomanager/Service/DashBoardService.java
@@ -2,7 +2,9 @@ package hei.phylosius.restomanager.Service;
 
 
 import hei.phylosius.restomanager.Repository.DashBoardDAO;
+import hei.phylosius.restomanager.dto.DishProcessingTimeRest;
 import hei.phylosius.restomanager.dto.DishSaleRest;
+import hei.phylosius.restomanager.dto.ProcessingTimeType;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,10 @@ import java.util.List;
 public class DashBoardService {
 
     private DashBoardDAO dashBoardDAO;
+
+    public DishProcessingTimeRest getProcessingTime(String dishId, ProcessingTimeType processingTimeType, LocalDateTime startDate, LocalDateTime endDate) {
+        return dashBoardDAO.getProcessingTime(dishId, processingTimeType, startDate, endDate);
+    }
 
     public List<DishSaleRest> getBestSales(Integer limit, LocalDateTime startDate, LocalDateTime endDate) {
         return dashBoardDAO.getBestSales(limit, startDate, endDate);
