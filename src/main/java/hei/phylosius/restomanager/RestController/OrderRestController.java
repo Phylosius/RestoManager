740025cefdc6e868d1ea28ce04ce742fc4e2f105
Phylosius <hei.phylosius@gmail.com>
2025-04-10 19:06:30 +0300

    feat: implemented GET /orders/{reference}

--- src/main/java/hei/phylosius/restomanager/RestController/OrderRestController.java
+++ src/main/java/hei/phylosius/restomanager/RestController/OrderRestController.java
@@ -1,23 +1,35 @@
 package hei.phylosius.restomanager.RestController;
 
+import hei.phylosius.restomanager.Repository.OrderNotFoundException;
+import hei.phylosius.restomanager.Service.OrderService;
 import hei.phylosius.restomanager.dto.DishOrderRestUpdate;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/orders")
 public class OrderRestController {
+
+    private OrderService orderService;
 
     @GetMapping("/{reference}")
     public ResponseEntity<?> getOrderInfo(
             @PathVariable String reference
     ) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            return ResponseEntity.ok(orderService.getOrderInfo(reference));
+        } catch (OrderNotFoundException e) {
+            return ResponseEntity.status(404).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
     }
 
-    @GetMapping("/{reference}/dishes")
+    @PutMapping("/{reference}/dishes")
     public ResponseEntity<?> updateDishes(
             @PathVariable String reference,
             @RequestBody List<DishOrderRestUpdate> dishes
