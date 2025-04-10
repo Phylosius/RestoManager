1897d7c40eb47bb98779abdc5e1656975248e982
Phylosius <hei.phylosius@gmail.com>
2025-04-11 00:46:49 +0300

    chore(dto): created DishProcessingTimeRest

---
+++ src/main/java/hei/phylosius/restomanager/dto/DishProcessingTimeRest.java
@@ -0,0 +1,9 @@
+package hei.phylosius.restomanager.dto;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class DishProcessingTimeRest {
+    private String processingTime;
+    private ProcessingTimeType processingTimeType;
+}
---
+++ src/main/java/hei/phylosius/restomanager/dto/ProcessingTimeType.java
@@ -0,0 +1,7 @@
+package hei.phylosius.restomanager.dto;
+
+public enum ProcessingTimeType {
+    MAXIMUM,
+    AVERAGE,
+    MINIMUM
+}
