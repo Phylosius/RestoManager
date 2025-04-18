855f1e7562b5d836b235646a83be671d05926d49
Phylosius <hei.phylosius@gmail.com>
2025-04-19 02:57:24 +0300

    feat(dto): create DTO for Make-up creation

---
+++ src/main/java/hei/phylosius/restomanager/dto/CreateDishIngredientRest.java
@@ -0,0 +1,14 @@
+package hei.phylosius.restomanager.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateDishIngredientRest extends IngredientBasicPropertyRest{
+    private Double requiredQuantity;
+}
